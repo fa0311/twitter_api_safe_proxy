4a2ddf8465c43ed6f9e71daa1cd171124dad2b9f
fa0311 <yuki@yuki0311.com>
2026-05-03 21:21:36 +0900

    update

--- injects/setup.js
+++ injects/setup.js
@@ -1,16 +1,97 @@
-// https://github.com/fa0311/twitter_api_browser_python/blob/main/twitter_api_browser_python/inject/setup.js
+// https://github.com/tsukumijima/KonomiTV/blob/master/server/static/zendriver_setup.js
 (async () => {
-  const __origApply = Function.prototype.apply;
-  const client = await new Promise((resolve) => {
-    Function.prototype.apply = function (thisArg, argsArray) {
-      if (thisArg && typeof thisArg === "object" && thisArg.dispatch === this) {
-        resolve(thisArg);
-      }
-      return __origApply.bind(this)(thisArg, argsArray);
-    };
-  });
-  Function.prototype.apply = __origApply;
-  globalThis.elonmusk_114514_request = async(query) => {
-    return client.dispatch.apply(client, [query]);
-  };
+	const objectMocker = async (target, name) => {
+		let value = target[name];
+
+		return await new Promise((resolve) => {
+			Object.defineProperty(target, name, {
+				configurable: true,
+				get: () => value,
+				set(nextValue) {
+					value = nextValue;
+					Object.defineProperty(target, name, {
+						configurable: true,
+						writable: true,
+						value: nextValue,
+					});
+					resolve(nextValue);
+				},
+			});
+		});
+	};
+
+	const chunkArray = await objectMocker(window, "webpackChunk_twitter_responsive_web");
+
+	const originalPush = chunkArray.push;
+	const client = await new Promise((resolve) => {
+		chunkArray.push = (chunk) => {
+			const modules = chunk[1];
+			if (modules && typeof modules === "object") {
+				for (const moduleId of Object.keys(modules)) {
+					const originalFactory = modules[moduleId];
+					modules[moduleId] = function (module, exports, require) {
+						const originalDefineExports = require.d;
+						require.d = (exp, definition) => {
+							for (const key in definition) {
+								Object.defineProperty(exp, key, {
+									enumerable: true,
+									configurable: true,
+									get: definition[key],
+								});
+							}
+						};
+						const result = originalFactory.apply(this, arguments);
+						require.d = originalDefineExports;
+
+						const propertyDescriptors = Object.getOwnPropertyDescriptors(module.exports);
+						for (const [exportKey, descriptor] of Object.entries(propertyDescriptors)) {
+							const originalClass = module.exports[exportKey];
+							if (typeof originalClass !== "function") {
+								continue;
+							}
+							if (originalClass.prototype === undefined) {
+								continue;
+							}
+							if (typeof originalClass.prototype.dispatch !== "function") {
+								continue;
+							}
+							if (typeof originalClass.prototype.get !== "function") {
+								continue;
+							}
+							if (typeof originalClass.prototype.post !== "function") {
+								continue;
+							}
+							if (typeof originalClass.prototype.delete !== "function") {
+								continue;
+							}
+
+							const constructionProxy = new Proxy(originalClass, {
+								construct(target, args, newTarget) {
+									const instance = Reflect.construct(target, args, newTarget);
+									resolve(instance);
+									return instance;
+								},
+							});
+
+							Object.defineProperty(module.exports, exportKey, {
+								enumerable: true,
+								configurable: true,
+								get: () => constructionProxy,
+							});
+						}
+
+						return result;
+					};
+				}
+			}
+			return originalPush(chunk);
+		};
+	});
+
+	console.log("Twitter API client found");
+
+	chunkArray.push = originalPush;
+	globalThis.elonmusk_114514_request = async (query) => {
+		return client[query[0]].apply(client, query.slice(1));
+	};
 })();
--- src/index.ts
+++ src/index.ts
@@ -32,26 +32,74 @@ const main = async () => {
 			userDataDir: profile.browser.userDataDir,
 		});
 
-		const op = twitter.getOperation("HomeTimeline");
-		try {
-			const res = await twitter.graphql({
-				method: op.getMethod(),
-				path: op.getPath(),
-				body: {
-					variables: {
-						count: 20,
-						includePromotedContent: true,
-						latestControlAvailable: true,
-						withCommunity: true,
-					},
-					queryId: op.getQueryId(),
-					features: op.getFeatures({}, "ignore"),
-					fieldToggles: op.createFieldToggles({}, "keep"),
+		console.log("Testing GraphQL request...");
+		const op = await twitter.graphql(
+			{
+				queryId: "jYMvLJJjGjO3aKWY3bP5HA",
+				operationName: "HomeTimeline",
+				operationType: "query",
+				metadata: {
+					featureSwitches: [
+						"rweb_video_screen_enabled",
+						"rweb_cashtags_enabled",
+						"profile_label_improvements_pcf_label_in_post_enabled",
+						"responsive_web_profile_redirect_enabled",
+						"rweb_tipjar_consumption_enabled",
+						"verified_phone_label_enabled",
+						"creator_subscriptions_tweet_preview_api_enabled",
+						"responsive_web_graphql_timeline_navigation_enabled",
+						"responsive_web_graphql_skip_user_profile_image_extensions_enabled",
+						"premium_content_api_read_enabled",
+						"communities_web_enable_tweet_community_results_fetch",
+						"c9s_tweet_anatomy_moderator_badge_enabled",
+						"responsive_web_grok_analyze_button_fetch_trends_enabled",
+						"responsive_web_grok_analyze_post_followups_enabled",
+						"responsive_web_jetfuel_frame",
+						"responsive_web_grok_share_attachment_enabled",
+						"responsive_web_grok_annotations_enabled",
+						"articles_preview_enabled",
+						"responsive_web_edit_tweet_api_enabled",
+						"graphql_is_translatable_rweb_tweet_is_translatable_enabled",
+						"view_counts_everywhere_api_enabled",
+						"longform_notetweets_consumption_enabled",
+						"responsive_web_twitter_article_tweet_consumption_enabled",
+						"content_disclosure_indicator_enabled",
+						"content_disclosure_ai_generated_indicator_enabled",
+						"responsive_web_grok_show_grok_translated_post",
+						"responsive_web_grok_analysis_button_from_backend",
+						"post_ctas_fetch_enabled",
+						"freedom_of_speech_not_reach_fetch_enabled",
+						"standardized_nudges_misinfo",
+						"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled",
+						"longform_notetweets_rich_text_read_enabled",
+						"longform_notetweets_inline_media_enabled",
+						"responsive_web_grok_image_annotation_enabled",
+						"responsive_web_grok_imagine_annotation_enabled",
+						"responsive_web_grok_community_note_auto_translation_is_enabled",
+						"responsive_web_enhance_cards_enabled",
+					],
+					fieldToggles: [
+						"withPayments",
+						"withAuxiliaryUserLabels",
+						"withArticleRichContentState",
+						"withArticlePlainText",
+						"withArticleSummaryText",
+						"withArticleVoiceOver",
+						"withGrokAnalyze",
+						"withDisallowedReplyControls",
+					],
 				},
-			});
-			await fs.writeFile(`output_${profile.name}_home_timeline.json`, JSON.stringify(res, null, 2), "utf-8");
-			console.log(JSON.stringify(res, null, 2));
-		} catch (e) {}
+			},
+			{
+				count: 20,
+				enableRanking: false,
+				includePromotedContent: true,
+				requestContext: "launch",
+				seenTweetIds: [],
+			},
+		);
+		logger.info("GraphQL response:");
+		logger.info(op);
 	}
 };
 
